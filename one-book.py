7d954f6c110557dabe3a51def21846f7160e6432
AH <allisha5812@yahoo.com>
2025-02-25 19:20:14 -0600

    one book title

--- one-book.py
+++ one-book.py
@@ -1,6 +1,13 @@
 import requests
+from bs4 import BeautifulSoup
 url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 page = requests.get(url)
 
+soup = BeautifulSoup(page.content, "html.parser")
+
 #See html source
-print(page.content)
+#print(page.content)
+
+titles=soup.find("h1").text
+print(titles)
+
